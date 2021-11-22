fa54882e5f9b2cc65178595d1b29a8aa2bd0a569
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-11-22 17:17:45 +0100

    Es. Pari e Dispari - Creato le 2 function

--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -5,3 +5,52 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+
+// chiedo all'utente se sceglie pari o dispari
+
+
+// chiedo all'utente che numero sceglie da 1 a 5
+
+
+// sommo il numero dell'utente a quello del pc
+
+
+// dichiaro il vincitore
+
+
+
+let ciao = casualNumber1To5();
+
+// FUNCTION
+// creo una funzione per generare un numero casuale da 1 a 5
+// da utilizzare senza inserire alcun argomento
+// return: fornisce un numero compreso tra 1 e 5 inclusi 1 e 5.
+function casualNumber1To5() {
+    const result = Math.floor(Math.random() * 5) + 1;
+
+    return result;
+}
+
+
+
+let userNumber = 2;
+console.log(userNumber);
+
+let numberEvenOrOdd = evenOrOdd(userNumber);
+console.log(numberEvenOrOdd);
+
+// FUNCTION
+// creo una funzione che mi dice se il numero è pari o dispari, fornendomi un risultato booleano.
+// number --> Numero da valutare
+// result: 'true' se è pari oppure 'false' se è dispari
+function evenOrOdd(number) {
+    let result = '';
+    if (number % 2 == 0) {
+        result = true
+    } else {
+        result = false
+    }
+
+    return result;
+}
