7ebb9a7e82738a78346a2bb0045f6ea69edeaa2f
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-11-22 16:12:58 +0100

    Es. Palindroma - Completato

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -5,22 +5,37 @@
 
 
 // Chiedo all'utente di inserire una parola
-let userWord = palindromeWord('ciao');
+let userWord = prompt(`Per vedere se una parola è polindroma inseriscila qui sotto e premi ok.`);
+let wordResult = palindromeWord(userWord);
+alert(wordResult);
 
 
 // Creo la funzione che mi permette di capire se la parola è palindroma
 
+// FUNCTION
+// Questa funzione permette di capire se la parola è palindroma oppure no con un risultato booleano.
+// Se è palindroma, avrà come risultato 'true', altrimenti 'false'.
+
+// word --> E' la parola da analizzare
+
+// return: 'true' se è palindroma, altrimenti 'false'.
+
 function palindromeWord(word) {
 
-    // Leggo la parola all'incontrario
-    let singleLetter;
+    // Leggo la parola al contrario
+    let invertedWord = '';
     for (let i = word.length - 1; i >= 0; i--) {
-        singleLetter = word[i];
-        console.log(word[i]);
+        invertedWord += word[i];
+    }
 
+
+    let result;
+    if (invertedWord === userWord) {
+        result = 'Questa parola è palindroma!'
+    } else {
+        result = 'Questa parola non è palindroma!'
     }
     
+    return result;
 
-    return singleLetter;
-    
 }
