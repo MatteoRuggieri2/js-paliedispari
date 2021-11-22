4f3e76e829449367bd64b850303a1e39a89bd5f8
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-11-22 15:24:27 +0100

    Es. Palindroma - ciclo for per legger al contrario

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -2,3 +2,25 @@
 
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+
+// Chiedo all'utente di inserire una parola
+let userWord = palindromeWord('ciao');
+
+
+// Creo la funzione che mi permette di capire se la parola è palindroma
+
+function palindromeWord(word) {
+
+    // Leggo la parola all'incontrario
+    let singleLetter;
+    for (let i = word.length - 1; i >= 0; i--) {
+        singleLetter = word[i];
+        console.log(word[i]);
+
+    }
+    
+
+    return singleLetter;
+    
+}
